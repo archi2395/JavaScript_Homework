6afb4c455f9188ab7992b97da87b43cd67472b94
Artemiy K <kurbatovartur40@gmail.com>
2026-03-02 13:33:17 +0200

    Added new repository

---
+++ homework_js3.2/script2.js
@@ -0,0 +1,5 @@
+const nameInput = prompt (`What is your name?`) ;
+const ageInput = prompt (`Enter your age:`) ;
+const workInput = prompt (`Enter your workplace:`) ;
+console.log(`${workInput} ${nameInput} ${ageInput}`);
+
